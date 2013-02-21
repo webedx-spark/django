2c0997e82864c4bc5769f4db667a270f367ca437
Bipin Suresh <bipin@coursera.org>
2013-02-20 21:02:11 -0800

    Typo

--- django/contrib/auth/admin.py
+++ django/contrib/auth/admin.py
@@ -38,7 +38,7 @@ class UserAdmin(admin.ModelAdmin):
     change_user_password_template = None
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
-        (_('Personal info'), {'fields': ('email')}),
+        (_('Personal info'), {'fields': ('email', )}),
         (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
                                        'groups', 'user_permissions')}),
         (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
